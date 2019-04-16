05b8e7670fec1530908d51895884a6637595b0e8
Kenny Wu <user2279779@yahoo.com>
2019-04-15 22:33:28 -0700

    api call

---
+++ components/Apod.vue
@@ -0,0 +1,40 @@
+<template>
+  <article>
+    <div>
+      <b-jumbotron header="Spotlight!" header-level="4" lead="Check it out, it's far out!">
+        <h2>{{ title }}</h2>
+        <div class="thumbnail" :style="{backgroundImage: 'url(' + url + ')'}"></div>
+        <p>{{ explanation }}</p>
+        <span>Credit: {{copyright}}</span>
+        <span>{{ date }}</span>
+      </b-jumbotron>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ["copyright", "date", "explanation", "url", "title"]
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: black;
+}
+.mission {
+  box-sizing: border-box;
+  width: auto;
+  padding: 10px;
+  border: 1px solid slategray;
+  box-shadow: 4px;
+  margin: 10px;
+}
+.thumbnail {
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 35em;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,39 @@
   <section class="container">
     <h1>Homepage</h1>
     <p class="subheading">welcome to the homepage</p>
-    <div>
-      <b-jumbotron header="Hello there!" lead="This is my nuxt page.">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore rerum molestiae vero a odio sunt vitae, ut, quae nam asperiores voluptate sequi ea voluptatum itaque tempore consectetur nostrum nisi perferendis!</p>
-      </b-jumbotron>
-    </div>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, deleniti? Iusto, magnam doloremque quaerat commodi vero quo rem recusandae corrupti non doloribus architecto, ipsum amet ut, quae quia eius excepturi!</p>
+    <Apod
+      v-for="(apod, index) in data"
+      :key="index"
+      :copyright="data.copyright"
+      :title="data.title"
+      :date="data.date"
+      :explanation="data.explanation"
+      :url="data.url"
+    />
   </section>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import axios from "axios";
+import Apod from "@/components/Apod";
+
 export default {
+  components: { Apod },
+  asyncData({ req, params }) {
+    return axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=THQPTFCIhCV3heTNCJGVkhjjYBcsdtejgBRKPtll"
+      )
+      .then(res => {
+        console.log(res.data);
+        return { data: res.data };
+      })
+      .catch(res => {
+        console.log(res);
+      });
+  },
   head() {
     return {
       title: "Welcome",
@@ -22,8 +43,7 @@ export default {
         { name: "keywords", content: "searchable terms" }
       ]
     };
-  },
-  components: {}
+  }
 };
 </script>
 
