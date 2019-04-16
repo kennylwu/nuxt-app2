5b953cbb1f9dd93b5910bcd3d99fa39411f3ef79
Kenny Wu <user2279779@yahoo.com>
2019-04-16 13:26:00 -0700

    fixed props

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,14 @@
-<template>
+<template v-on="(apod, index) in nasaData" :key="index">
   <section class="container">
     <h1>Homepage</h1>
     <p class="subheading">welcome to the homepage</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, deleniti? Iusto, magnam doloremque quaerat commodi vero quo rem recusandae corrupti non doloribus architecto, ipsum amet ut, quae quia eius excepturi!</p>
     <Apod
-      v-for="(apod, index) in data"
-      :key="index"
-      :copyright="data.copyright"
-      :title="data.title"
-      :date="data.date"
-      :explanation="data.explanation"
-      :url="data.url"
+      :copyright="nasaData.copyright"
+      :title="nasaData.title"
+      :date="nasaData.date"
+      :explanation="nasaData.explanation"
+      :url="nasaData.url"
     />
   </section>
 </template>
@@ -29,7 +27,8 @@ export default {
       )
       .then(res => {
         console.log(res.data);
-        return { data: res.data };
+        return { nasaData: res.data };
+        //this.data = res.data;
       })
       .catch(res => {
         console.log(res);
